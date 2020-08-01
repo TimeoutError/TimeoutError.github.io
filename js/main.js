fb1152176ed4e3d9576ffbb8f04854f31674edac
DLZR <65038406+TimeoutError@users.noreply.github.com>
2020-08-01 18:08:54 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,15 +3,17 @@ var	gl = null,
 		
 function resize_handler()
 {
-	canvas.width  = window.innerHeight;
-	canvas.height = window.innerWidth; 
+	gl.width  = window.innerHeight;
+	gl.height = window.innerWidth; 
 }
 
 window.onload = function()
 {
 	gl = canvas.getContext('webgl');
-	alert(`GL object: ${gl}`);
-	document.addEventListener('resize', resize_handler);
+	
+	$(window)
+	.on('resize', resize_handler)
+	.resize();
 	
 	if(gl)
 	{
