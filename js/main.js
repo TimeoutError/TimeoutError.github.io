6c1d2534f3fd99963e7726a6c07b55d8a7945bfc
DLZR <65038406+TimeoutError@users.noreply.github.com>
2020-08-01 17:52:34 +0100

    Adding basic WebGL usage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-	alert('Page loaded.');
-});
+var	gl = null,
+		canvas = $("#mainCanvas");
+		
+function resize_handler()
+{
+	canvas.width  = window.innerHeight;
+	canvas.height = window.innerWidth; 
+}
+
+window.onload = ()
+{
+	gl = canvas.getContext('webgl');
+	
+	document.addEventListener('resize', resize_handler);
+	
+	if(gl)
+	{
+		gl.clearColor(1.0, 1.0, 0.0, 1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT);
+	}
+	else
+	{
+		// Browser doesn't support WebGL.
+	}
+	
+};
